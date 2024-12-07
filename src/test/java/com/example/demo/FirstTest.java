d877098548f1d1074fecca6d0c6b3bae503541f6
G-mi <zxcvb17295@gmail.com>
2024-12-07 19:47:48 +0800

    first commit

---
+++ src/test/java/com/example/demo/FirstTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstTest {
+
+    @Test
+    void test_first() {
+        assertEquals(1, 1);
+    }
+}
