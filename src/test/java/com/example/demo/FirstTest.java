ab2887b80433d33e1b69e04c7454a922c048f65e
G-mi <zxcvb17295@gmail.com>
2024-12-07 20:47:13 +0800

    test4

--- src/test/java/com/example/demo/FirstTest.java
+++ src/test/java/com/example/demo/FirstTest.java
@@ -10,4 +10,20 @@ public class FirstTest {
     void test_first() {
         assertEquals(1, 1);
     }
+
+    void test_2() {
+        assertEquals(1, 1);
+    }
+    void test_3() {
+        assertEquals(1, 1);
+    }
+    void test_4() {
+        assertEquals(1, 1);
+    }
+    void test_5() {
+        assertEquals(2, 1);
+    }
+    void test_6() {
+        assertEquals(1, 1);
+    }
 }
